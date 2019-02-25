ad6faa6609f789f03ddee939d960b72ce3faf0f0
Mumo Nzinga <mnzinga888@gmail.com>
2019-02-25 09:26:28 +0300

    working changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,15 @@
-$(document).ready(function()) {
-  $("form#trianglemeasure").submit(function(event)  {
-    var side1 = parseInt($("input#side1").val());
-    var side2 = parseInt($("input#side2").val());
-    var side3 = parseInt($("input#side3").val());
-    var equilateral = "Equilateral";
-    var isosceles = "Isosceles";
-    var scalene = "Scalene";
-    var nottriangle = "NOT A TRIANGLE";
+function trianglemeasure(){
+  var side1=parseInt(document.getElementById('side1').value);
+  var side2=parseInt(document.getElementById('side2').value);
+  var side3=parseInt(document.getElementById('side3').value);
 
-
-    function triangle  (side1, side2, side3)
-
-    if (side1===side2===side3){
-      $("#real").text("This is an Equilateral Triangle");
-    } else if (side1===side2 || side2===side3 || side1===side3) {
-			$("#real").text("This is an Isosceles Triangle");
-    } else if (side1!==side2!==side3 && side1+side2>side3 || side2+side3>side1 || side1+side3>side2)
-			$("real").text("This is a scalene triangle");
-    } else {
-			$("real").text("NOT A TRIANGLE")
-    }
-  });
-});
+  if(side1==side2 && side2==side3){
+    alert("Equilateral Triangle");
+  } else if (side1==side2 || side2 == side3 || side3 == side1) {
+    alert("Isosceles Triangle");
+  } else if(side1 !=side2 && side2 !=side3){
+    alert("Scalene Triangle")
+  } else {
+    alert("Not a Triangle")
+  }
+}
